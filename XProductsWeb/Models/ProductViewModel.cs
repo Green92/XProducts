f45edeb923bcdd6ee62e17bf5fdeff1db2dbc769
Julien Kermes <julien.kermes@gmail.com>
2016-06-18 14:07:26 +0200

    Add XProductWeb ASP project

---
+++ XProductsWeb/Models/ProductViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Model.Entities;
+using System.Web.Mvc;
+
+namespace XProductsWeb.Models
+{
+    public class ProductViewModel
+    {
+        public List<Product> Products { get; set; }
+
+        public List<Category> Categories { get; set; }
+
+        public int SelectedCategory { get; set; }
+
+        public String Search { get; set; }
+
+        public IEnumerable<SelectListItem> CategoryItems
+        {
+            get {
+                List<Category> categories = new List<Category>();
+                categories.Add(new Category()
+                {
+                    Id = 0,
+                    Wording = "All"
+                });
+
+                if (Categories.Count > 0)
+                {
+                    categories.AddRange(Categories);
+                }
+
+                return new SelectList(categories, "Id", "Wording");
+            }
+        }
+
+        //public int SelectedCategoryId
+        //{
+        //    get {
+        //        int result = 0;
+        //        if (SelectedCategory != null)
+        //        {
+        //            int.TryParse(SelectedCategory.Value, out result);
+        //        }
+
+        //        return result;
+        //    }
+        //}
+    }
+}
